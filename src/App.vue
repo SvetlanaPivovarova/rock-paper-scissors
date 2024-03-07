c5a9c14e0813b77e8d0fb34e099f59adedaad09e
svetlana <fatinialux@yandex.ru>
2024-03-07 13:18:26 +0400

    feat: adaptive

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import HomeView from "@/views/HomeView.vue";
 </script>
 
 <template>
-  <div class="container">
+  <div class="container flex-column-container">
     <HomeView />
   </div>
 </template>
